b6a9985048f1fe436c15cace6969be7df74430c1
Joseph Ng <joseph_sieang@live.com>
2022-05-06 17:15:39 +0800

    Implement Animal class with getter and setter to flyingAbility

---
+++ strategy-pattern/Animal.java
@@ -0,0 +1,11 @@
+public class Animal {
+  private FlyingAbility flyingAbility;
+
+  public String getFlyingAbility() {
+    return flyingAbility.getFlyingAbility();
+  }
+
+  public void setFlyingAbility(FlyingAbility newFlyingAbility) {
+    flyingAbility = newFlyingAbility;
+  }
+}
