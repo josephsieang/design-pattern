83b1b5fddc80c70c80fc929d9ea87f8435ac081f
Joseph Ng <joseph_sieang@live.com>
2022-05-08 23:11:07 +0800

    Implement RobotPlan interface

---
+++ builder-pattern/RobotPlan.java
@@ -0,0 +1,17 @@
+public interface RobotPlan {
+  public void setRobotHead(String head);
+
+  public String getRobotHead();
+
+  public void setRobotTorso(String torso);
+
+  public String getRobotTorso();
+
+  public void setRobotArms(String arms);
+
+  public String getRobotArms();
+
+  public void setRobotLegs(String legs);
+
+  public String getRobotLegs();
+}
