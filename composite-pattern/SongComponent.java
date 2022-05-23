69d99ad1981033f6474f1c202124724025d58eb4
Joseph Ng <joseph_sieang@live.com>
2022-05-23 08:59:57 +0800

    Implement SongComponent abstract class

---
+++ composite-pattern/SongComponent.java
@@ -0,0 +1,29 @@
+public abstract class SongComponent {
+  public void add(SongComponent songComponent) {
+    throw new UnsupportedOperationException();
+  }
+
+  public void remove(SongComponent songComponent) {
+    throw new UnsupportedOperationException();
+  }
+
+  public SongComponent getSongComponent(int idx) {
+    throw new UnsupportedOperationException();
+  }
+
+  public String getSongName() {
+    throw new UnsupportedOperationException();
+  }
+
+  public String getBandName() {
+    throw new UnsupportedOperationException();
+  }
+
+  public int getYearReleased() {
+    throw new UnsupportedOperationException();
+  }
+
+  public void displaySongInfo() {
+    throw new UnsupportedOperationException();
+  }
+}
